509585d7d7e879b442bc45d69df55d4c5e948fbd
DinDin <71533874+6184dindin@users.noreply.github.com>
2020-12-16 14:32:37 +0700

    CreateOrderActivity and HomeCommodityOwnerActivity

--- app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/CreateOrderActivity.java
+++ app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/CreateOrderActivity.java
@@ -3,7 +3,11 @@ package com.vicarry.vicarryproject.ui.commodityowner;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.view.View;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.Toast;
 
 import com.vicarry.vicarryproject.R;
 import com.vicarry.vicarryproject.databinding.ActivityCreateOrderBinding;
@@ -15,5 +19,41 @@ public class CreateOrderActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_create_order);
+        createOrder();
+    }
+    private void createOrder() {
+        binding.layoutInfo1.setVisibility(View.VISIBLE);
+        binding.layoutInfo2.setVisibility(View.GONE);
+        binding.btnComplete.setText("TIẾP THEO");
+
+        binding.btnBack.setOnClickListener(v -> {
+            finish();
+        });
+        binding.btnComplete.setOnClickListener(v -> {
+            closeKeyboard();
+            completeOrder();
+        });
+    }
+
+    private void completeOrder() {
+        binding.layoutInfo1.setVisibility(View.GONE);
+        binding.layoutInfo2.setVisibility(View.VISIBLE);
+        binding.btnComplete.setText("HOÀN THÀNH");
+
+        binding.btnBack.setOnClickListener(v -> {
+            closeKeyboard();
+            createOrder();
+        });
+        binding.btnComplete.setOnClickListener(v-> {
+            closeKeyboard();
+            Toast.makeText(this, "Compelte", Toast.LENGTH_SHORT).show();
+        });
+    }
+    private void closeKeyboard() {
+        View view = this.getCurrentFocus();
+        if(view != null) {
+            InputMethodManager inputMethodManager = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
+            inputMethodManager.hideSoftInputFromWindow(view.getWindowToken(), 0);
+        }
     }
 }
---
+++ app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/adapter/OrderAdapter.java
@@ -0,0 +1,57 @@
+package com.vicarry.vicarryproject.ui.commodityowner.adapter;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.databinding.DataBindingUtil;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.vicarry.vicarryproject.R;
+import com.vicarry.vicarryproject.databinding.ItemOrderBinding;
+import com.vicarry.vicarryproject.ui.commodityowner.model.Order;
+
+import java.util.List;
+
+public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> {
+    Context context;
+    List<Order> orders;
+
+    public OrderAdapter(Context context, List<Order> orders) {
+        this.context = context;
+        this.orders = orders;
+    }
+
+    @NonNull
+    @Override
+    public OrderAdapter.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        LayoutInflater inflater = LayoutInflater.from(parent.getContext());
+        ItemOrderBinding binding = DataBindingUtil.inflate(inflater, R.layout.item_order, parent, false);
+        return new ViewHolder(binding);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull OrderAdapter.ViewHolder holder, int position) {
+        Order order = orders.get(position);
+
+        holder.binding.id.setText(String.valueOf(order.getId()));
+        if(position == orders.size() - 1) {
+            holder.binding.layoutMarginBottom.setVisibility(View.VISIBLE);
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return orders.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        ItemOrderBinding binding;
+        public ViewHolder(@NonNull ItemOrderBinding binding1) {
+            super(binding1.getRoot());
+            binding = binding1;
+        }
+    }
+}
