d80ca7bbea33c11f408fa81bade8bec2f987fc9e
DinDin <71533874+6184dindin@users.noreply.github.com>
2020-12-17 01:16:03 +0700

    create OrderInformationActivity

--- app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/OrderInformationActivity.java
+++ app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/OrderInformationActivity.java
@@ -4,6 +4,12 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.transition.AutoTransition;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import com.vicarry.vicarryproject.R;
 import com.vicarry.vicarryproject.databinding.ActivityOrderInformationBinding;
@@ -15,5 +21,22 @@ public class OrderInformationActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_order_information);
+        seeDetails();
+    }
+
+    private void seeDetails() {
+        TransitionManager.beginDelayedTransition(binding.layoutInformation, new AutoTransition().setDuration(500));
+        binding.layoutInfo1.setVisibility(View.VISIBLE);
+        binding.layoutInfo2.setVisibility(View.GONE);
+        binding.btnSeeDetails.setOnClickListener(v -> showMap());
+        binding.btnBack.setOnClickListener(v -> finish());
+    }
+
+    private void showMap() {
+        TransitionManager.beginDelayedTransition(binding.layoutInformation, new AutoTransition().setDuration(500));
+        binding.layoutInfo1.setVisibility(View.GONE);
+        binding.layoutInfo2.setVisibility(View.VISIBLE);
+        binding.btnShowMap.setOnClickListener(v -> seeDetails());
+        binding.btnBack.setOnClickListener(v -> seeDetails());
     }
 }
--- app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/adapter/OrderAdapter.java
+++ app/src/main/java/com/vicarry/vicarryproject/ui/commodityowner/adapter/OrderAdapter.java
@@ -1,6 +1,7 @@
 package com.vicarry.vicarryproject.ui.commodityowner.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -11,6 +12,7 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import com.vicarry.vicarryproject.R;
 import com.vicarry.vicarryproject.databinding.ItemOrderBinding;
+import com.vicarry.vicarryproject.ui.commodityowner.OrderInformationActivity;
 import com.vicarry.vicarryproject.ui.commodityowner.model.Order;
 
 import java.util.List;
@@ -37,6 +39,10 @@ public class OrderAdapter extends RecyclerView.Adapter<OrderAdapter.ViewHolder> 
         Order order = orders.get(position);
 
         holder.binding.id.setText(String.valueOf(order.getId()));
+        holder.binding.layoutItem.setOnClickListener(v -> {
+            Intent intent = new Intent(context, OrderInformationActivity.class);
+            context.startActivity(intent);
+        });
         if(position == orders.size() - 1) {
             holder.binding.layoutMarginBottom.setVisibility(View.VISIBLE);
         }
